819644bdbd639c162daf6f7cc3cd381dfa73b7b7
jhosep jimenez <jhosep.jimenez@ucr.ac.cr>
2022-11-10 00:39:00 -0600

    modulo basicamente listo

--- lanco-front/src/component/ProductForm.js
+++ lanco-front/src/component/ProductForm.js
@@ -1,29 +1,235 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { addProduct } from "../services/ProductService";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { getCurrentDate } from "./utils/date";
 
+const initialValues = {
+  base_pintura: "",
+  precio: "",
+  area_aplicacion: "",
+  fecha_caducidad: "",
+  color_pintura: "",
+  cantidad_stock: 0,
+  descripcion: "",
+  marca: "",
+  max: 0,
+  min: 0,
+};
+
+const options = [
+  { value: "", text: "--Elija una opcion--" },
+  { value: "Agua", text: "Agua " },
+  { value: "Aceite", text: "Aceite " },
+  { value: "Acrílica ", text: "Acrílica " },
+];
+const options2 = [
+  { value: "", text: "--Elija una opcion--" },
+  { value: "Pared", text: "Pared " },
+  { value: "Techo", text: "Techo " },
+  { value: "Hierro ", text: "Hierro " },
+];
 export default function ProductForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const [values, setValues] = useState(initialValues);
+  const [selected, setSelected] = useState(options[0].value);
+  const [selected2, setSelected2] = useState(options2[0].value);
+  const [error, setError] = useState({ fields: {}, errors: {} });
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    console.log(error.errors);
+    if (e.target.name === "base_pintura") {
+      setSelected(e.target.value);
+    }
+
+    if (e.target.name === "area_aplicacion") {
+      setSelected2(e.target.value);
+    }
+
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  function handleValidation() {
+    let fields = values;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!fields.descripcion.trim()) {
+      formIsValid = false;
+      errors["descripcion"] = "            No puede ser vacio";
+    }
+    if (!fields.color_pintura.trim()) {
+      formIsValid = false;
+      errors["color_pintura"] = "            No puede ser vacio";
+    }
+    if (!fields.marca.trim()) {
+      formIsValid = false;
+      errors["marca"] = " No puede ser vacio";
+    }
+    if (!fields.base_pintura) {
+      formIsValid = false;
+      errors["base_pintura"] = " Escoja una opcion";
+    }
+    if (!fields.area_aplicacion) {
+      formIsValid = false;
+      errors["area_aplicacion"] = " Escoja una opcion";
+    }
+    if (!fields.area_aplicacion) {
+      formIsValid = false;
+      errors["area_aplicacion"] = " Escoja una opcion";
+    }
+    var d1 = new Date(fields.fecha_caducidad);
+    var d2 = new Date();
+
+    if (d1.getTime() < d2.getTime()) {
+      formIsValid = false;
+      errors["fecha_caducidad"] = " Fecha no puede ser menor al dia Actual";
+    }
+
+    if (
+      Number(fields.cantidad_stock) > Number(fields.max) ||
+      Number(fields.cantidad_stock) < Number(fields.min)
+    ) {
+      formIsValid = false;
+      errors["cantidad_stock"] =
+        " El stock no puede ser mayor al max ni menor al min";
+    }
+
+    setError({ errors: errors });
+    return formIsValid;
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      console.log(values);
+      addProduct(values);
+      navigate("/productos");
+    }
+    /*
+     */
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="undefined" placeholder="Base Pintura" {...register} />
-      <input type="undefined" placeholder="Descripcion" {...register} />
-      <input type="undefined" placeholder="Área de aplicación" {...register} />
-      <input type="undefined" placeholder="Fecha Caducidad" {...register} />
-      <input type="undefined" placeholder="Color de pintura" {...register} />
-      <input type="undefined" placeholder="Marca" {...register} />
-      <input type="undefined" placeholder="Precio" {...register} />
-      <input type="undefined" placeholder="Cantidad en stock" {...register} />
-      <input type="undefined" placeholder="Max" {...register} />
-      <input type="undefined" placeholder="Min" {...register} />
+    <div>
+      <form id="msform" onSubmit={handleOnSubmit}>
+        <fieldset>
+          <label>Base pintura</label>
+          <span style={{ color: "red" }}>{error.errors["base_pintura"]}</span>
+          <select
+            name="base_pintura"
+            value={selected}
+            onChange={handleInputChange}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.text}
+              </option>
+            ))}
+          </select>
 
-      <input type="submit" />
-    </form>
+          <label>Area Aplicacion</label>
+          <span style={{ color: "red" }}>
+            {error.errors["area_aplicacion"]}
+          </span>
+          <select
+            name="area_aplicacion"
+            value={selected2}
+            onChange={handleInputChange}
+          >
+            {options2.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.text}
+              </option>
+            ))}
+          </select>
+          <label>Color pintura</label>
+          <span style={{ color: "red" }}>{error.errors["color_pintura"]}</span>
+          <input
+            value={values.color_pintura}
+            onChange={handleInputChange}
+            name="color_pintura"
+            required
+          />
+          <label>Descripcion</label>
+          <span style={{ color: "red" }}>{error.errors["descripcion"]}</span>
+          <input
+            value={values.descripcion}
+            onChange={handleInputChange}
+            name="descripcion"
+            required
+          />
+
+          <label>Fecha caducidad</label>
+          <span style={{ color: "red" }}>
+            {error.errors["fecha_caducidad"]}
+          </span>
+          <input
+            value={values.fecha_caducidad}
+            onChange={handleInputChange}
+            name="fecha_caducidad"
+            type="date"
+            required
+          />
+          <label>Precio</label>
+          <input
+            value={values.precio}
+            onChange={handleInputChange}
+            name="precio"
+            type="number"
+            required
+          />
+          <label>Marca</label>
+          <span style={{ color: "red" }}>{error.errors["marca"]}</span>
+          <input
+            value={values.marca}
+            onChange={handleInputChange}
+            name="marca"
+            required
+          />
+          <label>Cantidad en Stock</label>
+          <span style={{ color: "red" }}>{error.errors["cantidad_stock"]}</span>
+          <input
+            value={values.cantidad_stock}
+            onChange={handleInputChange}
+            name="cantidad_stock"
+            min="0"
+            type="number"
+            required
+          />
+          <label>Max</label>
+          <input
+            value={values.max}
+            onChange={handleInputChange}
+            name="max"
+            min="0"
+            type="number"
+            required
+          />
+
+          <label>Min</label>
+          <input
+            value={values.min}
+            onChange={handleInputChange}
+            name="min"
+            min="0"
+            type="number"
+            required
+          />
+
+          <button class="next action-button" disabled={false}>
+            {" "}
+            Registrar{" "}
+          </button>
+        </fieldset>
+      </form>
+    </div>
   );
 }
--- lanco-front/src/services/ProductService.js
+++ lanco-front/src/services/ProductService.js
@@ -37,3 +37,24 @@ export function deleteProduct(id) {
       console.error(error);
     });
 }
+
+export function addProduct(datas) {
+  console.log(datas);
+  const url = "http://localhost/proyecto-lanco-codigo/controller_productos.php";
+  const options = {
+    method: "POST",
+    withCredentials: false,
+    url: "http://localhost/proyecto-lanco-codigo/controller_productos.php",
+    data: datas,
+  };
+
+  return axios
+    .request(options)
+    .then((response) => {
+      const res = response.data;
+      return res;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
